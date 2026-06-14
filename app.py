273b32351ad98f813cce8fd5c4cfb3306b4099e6
Deepika GMB <gmbdeepika@gmail.com>
2026-06-15 01:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,5 @@ def predict():
     return render_template('predict.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
